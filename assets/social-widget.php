fa714b7d6382e617ba7da1776e8d70f86ee5e90d
Sophie Edqvist <mistyteahouse@gmail.com>
2015-12-27 19:46:02 +0100

    fixed links

--- assets/social-widget.php
+++ assets/social-widget.php
@@ -58,7 +58,7 @@ class Follow_Buttons extends WP_Widget {
     }
 
     if ( get_theme_mod('tumblr_profile') != '' ) {
-      echo '<a href="https://www.' . get_theme_mod('tumblr_profile') . 'tumblr.com/" target="_blank" class="follow tumblr">';
+      echo '<a href="https://' . get_theme_mod('tumblr_profile') . '.tumblr.com/" target="_blank" class="follow tumblr">';
       echo '<img src="' . get_stylesheet_directory_uri() . '/img/tumblr-icon.png" alt="tumblr icon" class="follow-icon">';
       echo '<span class="follow-name">';
       echo get_theme_mod('tumblr_profile');
